990a59d16269d18bb4a3f125f04ff6cc841ca7f0
Swarnima Tiwari <43717476+swarnima14@users.noreply.github.com>
2020-07-28 22:44:21 +0530

    ui changed

--- app/src/main/java/com/example/hp_awareness_app/VerifyPhoneActivity.java
+++ app/src/main/java/com/example/hp_awareness_app/VerifyPhoneActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alimuzaffar.lib.pin.PinEntryEditText;
 import com.google.android.gms.tasks.TaskExecutors;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,12 +36,7 @@ public class VerifyPhoneActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
 
-    private EditText et1;
-    private EditText et2;
-    private EditText et3;
-    private EditText et4;
-    private EditText et5;
-    private EditText et6;
+    private PinEntryEditText pinEntryEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,77 +53,19 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
 
         progressBar = findViewById(R.id.progressBar2);
+        pinEntryEditText=findViewById(R.id.etCode);
 
-        et1 = findViewById(R.id.et_1);
-        et2 = findViewById(R.id.et_2);
-        et3 = findViewById(R.id.et_3);
-        et4 = findViewById(R.id.et_4);
-        et5 = findViewById(R.id.et_5);
-        et6 = findViewById(R.id.et_6);
-
-
-        et1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!s.toString().equals("")) {
-                    et2.requestFocus();
-                }
-
-            }
-        });
-
-        changeTextViewFocus(et1, et2, et3);
-        changeTextViewFocus(et2, et3, et4);
-        changeTextViewFocus(et3, et4, et5);
-        changeTextViewFocus(et4, et5, et6);
-
-        et6.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.toString().equals("")) {
-                    et5.requestFocus();
-                }
-
-            }
-        });
 
         String phonenumber = getIntent().getStringExtra("phonenumber");
         sendVerificationCode(phonenumber);
 
-        Button buttonSignIn = findViewById(R.id.buttonSignIn);
+        Button buttonSignIn = findViewById(R.id.btnVerify);
         buttonSignIn.setOnClickListener(v -> {
 
-            String code = et1.getText().toString().trim()
-                    + et2.getText().toString().trim()
-                    + et3.getText().toString().trim()
-                    + et4.getText().toString().trim()
-                    + et5.getText().toString().trim()
-                    + et6.getText().toString().trim();
+            String code = pinEntryEditText.getText().toString().trim();
 
             if (code.isEmpty() || code.length() < 6) {
-
-                et1.setError("Enter code...");
-                et1.requestFocus();
+                pinEntryEditText.setError("Enter code...");
                 return;
             }
             verifyCode(code);
@@ -135,30 +73,6 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
     }
 
-    private void changeTextViewFocus(EditText et1, EditText et2, EditText et3) {
-        et2.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-                if (!s.toString().equals("")) {
-                    et3.requestFocus();
-                } else {
-                    et1.requestFocus();
-                }
-
-            }
-        });
-    }
 
     private void verifyCode(String code) {
         PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
@@ -205,33 +119,8 @@ public class VerifyPhoneActivity extends AppCompatActivity {
         public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
             String code = phoneAuthCredential.getSmsCode();
             if (code != null) {
-                int codeint = Integer.parseInt(code);
-
-                int six = codeint % 10;
-                int five = (codeint % 100) / 10;
-                int four = (codeint % 1000) / 100;
-                int three = (codeint % 10000) / 1000;
-                int two = (codeint % 100000) / 10000;
-                int one = (codeint) / 100000;
-
-                et1.setText(String.valueOf(one));
-                et2.setText(String.valueOf(two));
-                et3.setText(String.valueOf(three));
-                et4.setText(String.valueOf(four));
-                et5.setText(String.valueOf(five));
-                et6.setText(String.valueOf(six));
-
+                pinEntryEditText.setText(code);
                 verifyCode(code);
-            } else {
-
-                et1.setText("1");
-                et2.setText("2");
-                et3.setText("3");
-                et4.setText("4");
-                et5.setText("5");
-                et6.setText("6");
-
-                signInWithCredential(phoneAuthCredential);
             }
         }
 
@@ -247,10 +136,5 @@ public class VerifyPhoneActivity extends AppCompatActivity {
             Toast.makeText(VerifyPhoneActivity.this, "Verification Code Sent", Toast.LENGTH_SHORT).show();
         }
 
-        @Override
-        public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
-            super.onCodeAutoRetrievalTimeOut(s);
-            Toast.makeText(VerifyPhoneActivity.this, "Auto Retrieval Timeout, Enter code manually", Toast.LENGTH_SHORT).show();
-        }
     };
 }
